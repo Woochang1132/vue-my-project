dd60840f62f83c74becebe8de43e02d394165ccb
Woochang1132 <show4031132@gmail.com>
2024-05-04 12:18:30 +0900

    my-vue-project => 개인 프로젝트 시작 셋팅

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Calc></Calc>
+  </div>
+</template>
+
+<script>
+import Calc from "./components/Calc.vue"
+
+export default{
+  name: "App",
+  components : {Calc} 
+  
+}
+</script>
+
+<style scoped></style>
