b7f58b4dfdc85dde68441c2a730a1d481cba896d
Woochang1132 <show4031132@gmail.com>
2024-05-04 12:47:49 +0900

    my-vue-project => 반응형 함수 만들기(composition API)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <Calc></Calc>
+    <Calc2></Calc2>
   </div>
 </template>
 
 <script>
-import Calc from "./components/Calc.vue"
+// import Calc from "./components/Calc.vue"
+import Calc2 from "./components/Calc2.vue"
 
 export default{
   name: "App",
-  components : {Calc} 
+  components : {Calc2} 
   
 }
 </script>
