9f3a92172a743d5c2a52b158b489fb8eebc58d58
Woochang1132 <show4031132@gmail.com>
2024-05-04 12:59:40 +0900

    my-vue-project : reactive 반응성 적용하기

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <Calc2></Calc2>
+    <Calc3 />
   </div>
 </template>
 
 <script>
 // import Calc from "./components/Calc.vue"
-import Calc2 from "./components/Calc2.vue"
+// import Calc2 from "./components/Calc2.vue"
+import Calc3 from "./components/Calc3.vue"
 
 export default{
   name: "App",
-  components : {Calc2} 
+  components : {Calc3} 
   
 }
 </script>
