a7030b772ff677e4c1021747b4c2dc456b7ab632
Woochang1132 <show4031132@gmail.com>
2024-05-04 17:09:23 +0900

    my-vue-project => composition 방식으로 todoList 구성해보기

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
-  <div>
+  <div class="container">
+      <div class="card card-body bg-light">
+        <div classe="title">:: TodoList App</div>
+      </div>
+      <div class="card card-default card-borderless">
+          <div class="card-body">
+              <InputTodo @add-todo="addTodo"></InputTodo>
+              <TodoListCopy :todoList="state.todoList" @delete-todo="deleteTodo"
+              @toggle-completed="toggleCompleted"></TodoListCopy>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-// import Calc from "./components/Calc.vue"
-// import Calc2 from "./components/Calc2.vue"
-// import Calc3 from "./components/Calc3.vue"
-// import Calc4 from "./components/Calc4.vue"
-// import Calc5 from "./components/Calc5.vue"
-// import Calc8 from "./components/Calc8.vue"
+import {reactive, onMounted} from 'vue'
+import TodoListCopy from '../src/components/TodoListCopy.vue';
+import InputTodo from '../src/components/InputTodo.vue';
+
 
 export default{
-  name: "App",
-  
+    name: "App",
+    components: { TodoListCopy, InputTodo },
+    setup() {
+        const ts = new Date().getTime();
+        const state = reactive({ todoList: [] });
+        onMounted(() => {
+            state.todoList.push({ id: ts, todo: "자전거 타기", completed: false });
+            state.todoList.push({ id: ts + 1, todo: "딸과 공원 산책", completed: true });
+            state.todoList.push({ id: ts + 2, todo: "로스트아크 하기", completed: false });
+            state.todoList.push({ id: ts + 3, todo: "Vue 원고 집필", completed: false });
+        });
+        const addTodo = (todo) => {
+            if (todo.length >= 2) {
+                state.todoList.push({ id: new Date().getTime(),
+                    todo: todo, completed: false });
+            }
+        };
+        const deleteTodo = (id) => {
+            let index = state.todoList.findIndex((item) => id === item.id);
+            state.todoList.splice(index, 1);
+        };
+        const toggleCompleted = (id) => {
+            let index = state.todoList.findIndex((item) => id === item.id);
+            state.todoList[index].completed = !state.todoList[index].completed;
+        };
+        return { state, addTodo, deleteTodo, toggleCompleted };
+    },
+
 }
 </script>
 
